77be1e23bd92ab5d02d08e37d0c094bacda00223
Toby Murray <murray.toby@gmail.com>
2020-03-19 20:29:02 -0400

    WIP

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -160,7 +160,7 @@ void task_bme280_normal_mode(void *ignore) {
   }
 
   while (true) {
-    vTaskDelay(40 / portTICK_PERIOD_MS);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
 
     struct bme280_data data;
     result = bme280_get_sensor_data(BME280_ALL, &data, &bme280);
@@ -172,80 +172,14 @@ void task_bme280_normal_mode(void *ignore) {
     } else if (result > 0) {
       ESP_LOGW(TAG_BME280, "BME280 get sensor data warning: %d", result);
     } else {
-      ESP_LOGE(TAG_BME280, "BME@*) got sensor data error: %d", result);
+      ESP_LOGE(TAG_BME280, "BME280 got sensor data error: %d", result);
     }
   }
-
-  int32_t v_uncomp_pressure_int32_t;
-  int32_t v_uncomp_temperature_int32_t;
-  int32_t v_uncomp_humidity_int32_t;
-
-  //   if (com_rslt == SUCCESS) {
-  //     while (true) {
-  //       vTaskDelay(40 / portTICK_PERIOD_MS);
-
-  //       com_rslt = bme280_read_uncomp_pressure_temperature_humidity(
-  //           &v_uncomp_pressure_int32_t, &v_uncomp_temperature_int32_t, &v_uncomp_humidity_int32_t);
-
-  //       if (com_rslt == SUCCESS) {
-  //         ESP_LOGI(TAG_BME280, "%.2f degC / %.3f hPa / %.3f %%",
-  //                  bme280_compensate_temperature_double(v_uncomp_temperature_int32_t),
-  //                  bme280_compensate_pressure_double(v_uncomp_pressure_int32_t) / 100,  // Pa -> hPa
-  //                  bme280_compensate_humidity_double(v_uncomp_humidity_int32_t));
-  //       } else {
-  //         ESP_LOGE(TAG_BME280, "measure error. code: %d", com_rslt);
-  //       }
-  //     }
-  //   } else {
-  //     ESP_LOGE(TAG_BME280, "init or setting error. code: %d", com_rslt);
-  //   }
 
   vTaskDelete(NULL);
 }
 
-// void task_bme280_forced_mode(void *ignore) {
-// 	struct bme280_t bme280 = {
-// 		.bus_write = BME280_I2C_bus_write,
-// 		.bus_read = BME280_I2C_bus_read,
-// 		.dev_addr = BME280_I2C_ADDRESS2,
-// 		.delay_msec = BME280_delay_msek
-// 	};
-
-// 	int32_t com_rslt;
-// 	int32_t v_uncomp_pressure_int32_t;
-// 	int32_t v_uncomp_temperature_int32_t;
-// 	int32_t v_uncomp_humidity_int32_t;
-
-// 	com_rslt = bme280_init(&bme280);
-
-// 	com_rslt += bme280_set_oversamp_pressure(BME280_OVERSAMP_1X);
-// 	com_rslt += bme280_set_oversamp_temperature(BME280_OVERSAMP_1X);
-// 	com_rslt += bme280_set_oversamp_humidity(BME280_OVERSAMP_1X);
-
-// 	com_rslt += bme280_set_filter(BME280_FILTER_COEFF_OFF);
-// 	if (com_rslt == SUCCESS) {
-// 		while(true) {
-// 			com_rslt = bme280_get_forced_uncomp_pressure_temperature_humidity(
-// 				&v_uncomp_pressure_int32_t, &v_uncomp_temperature_int32_t, &v_uncomp_humidity_int32_t);
-
-// 			if (com_rslt == SUCCESS) {
-// 				ESP_LOGI(TAG_BME280, "%.2f degC / %.3f hPa / %.3f %%",
-// 					bme280_compensate_temperature_double(v_uncomp_temperature_int32_t),
-// 					bme280_compensate_pressure_double(v_uncomp_pressure_int32_t)/100, // Pa -> hPa
-// 					bme280_compensate_humidity_double(v_uncomp_humidity_int32_t));
-// 			} else {
-// 				ESP_LOGE(TAG_BME280, "measure error. code: %d", com_rslt);
-// 			}
-// 		}
-// 	} else {
-// 		ESP_LOGE(TAG_BME280, "init or setting error. code: %d", com_rslt);
-// 	}
-
-// 	vTaskDelete(NULL);
-// }
-
 void app_main(void) {
   i2c_master_init();
   xTaskCreate(&task_bme280_normal_mode, "bme280_normal_mode", 2048, NULL, 6, NULL);
-  // xTaskCreate(&task_bme280_forced_mode, "bme280_forced_mode",  2048, NULL, 6, NULL);
 }
